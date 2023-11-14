0f88efe25d740f5998f63642afbfdd2a0b7662e2
simon w <simon.westcott@atos.net>
2023-11-14 12:24:26 +0000

    Modified folders and fixed upload views

--- app/routes.js
+++ app/routes.js
@@ -9,26 +9,26 @@ const router = govukPrototypeKit.requests.setupRouter()
 // Add your routes here
 
 // Sign in
-router.post("/laps/sign-in",  function(request, response) {
-  response.redirect(301, "/laps/account-home-blk")
+router.post("/laps/admin/sign-in",  function(request, response) {
+  response.redirect(301, "/laps/admin/account-home-blk")
 });
 
 // account home blank
-router.post("/laps/account-home-blk",  function(request, response) {
-  response.redirect(301, "/laps/upload")
+router.post("/laps/admin/account-home-blk",  function(request, response) {
+  response.redirect(301, "/laps/admin/upload")
 });
 
 // upload
-router.post("/laps/upload",  function(request, response) {
-  response.redirect(301, "/laps/success")
+router.post("/laps/admin/upload?anerror=false&uploaderror=0&",  function(request, response) {
+response.redirect(301, "/laps/admin/success")
 });
 
 // change name
-router.post("/laps/barnsley-cd",  function(request, response) {
-  response.redirect(301, "/laps/barnsley-bcs")
+router.post("/laps/admin/barnsley-cd",  function(request, response) {
+  response.redirect(301, "/laps/admin/barnsley-bcs")
 });
 
 // change name
-router.post("/laps/barnsley-bcs",  function(request, response) {
-  response.redirect(301, "/laps/barnsley-bcs")
+router.post("/laps/admin/barnsley-bcs",  function(request, response) {
+  response.redirect(301, "/laps/admin/barnsley-bcs")
 });
