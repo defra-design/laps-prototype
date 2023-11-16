7cc93a0d3a9ed332ae6969c4b70cbd56b2637adf
simon w <simon.westcott@atos.net>
2023-11-16 14:08:54 +0000

    updates following feedback from CX team meeting

--- app/routes.js
+++ app/routes.js
@@ -53,8 +53,14 @@ router.post("/laps/account/create-password",  function(request, response) {
   if (chooseAnEvent === "finance and waste") {
     res.redirect("/laps/account/role-details");
   }
+// Cant do this right now
+  if (chooseAnEvent === "cannot") {
+    res.redirect("/laps/account/success-c");
+  }
 
 });
+
+
 
 // Add details
 router.post("/laps/account/role-details",  function(request, response) {
@@ -67,7 +73,7 @@ router.post("/laps/account/role-details",  function(request, response) {
     console.log(req.body);
 
     if (req.session.upload_another === "Yes") {
-      res.redirect(301, "/laps/account/responsibility");
+      res.redirect(301, "/laps/account/responsibility-2");
     }
  
     else {
@@ -85,6 +91,41 @@ router.post("/laps/account/check-details",  function(request, response) {
   response.redirect(301, "/laps/account/account-home")
 });
 
+//
+//
+// 2nd person addeed
+//
+//
+
+// Choose who will manage [name]'s EPR account
+//
+// What is this person's area of responsibility?
+router.post("/laps/account/responsibility-2", function (req, res) {
+  var chooseAnEvent = req.session.data["who_list_2"];
+
+// Finance role
+  if (chooseAnEvent === "finance")  {
+    res.redirect("/laps/account/role-details-2");
+  }
+// Waste role
+  if (chooseAnEvent === "waste") {
+    res.redirect("/laps/account/role-details-2");
+  }
+// Finance and waste role 
+  if (chooseAnEvent === "finance and waste") {
+    res.redirect("/laps/account/role-details-2");
+  }
+// Cant do this right now
+  if (chooseAnEvent === "cannot") {
+    res.redirect("/laps/account/check-details");
+  }
+
+});
+
+// Add details
+router.post("/laps/account/role-details-2",  function(request, response) {
+  response.redirect(301, "/laps/account/check-details")
+});
 
 
 
