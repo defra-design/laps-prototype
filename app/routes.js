0dd23d56bfdceb0b91c1f5dd260d586066a2808f
simon w <simon.westcott@atos.net>
2023-11-16 09:49:46 +0000

    draft version for Content designer

--- app/routes.js
+++ app/routes.js
@@ -8,6 +8,93 @@ const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
 
+//
+//
+// LA Account
+//
+//
+
+
+// Set up
+router.post("/laps/account/set-up",  function(request, response) {
+  response.redirect(301, "/laps/account/email")
+});
+
+// Email
+router.post("/laps/account/email",  function(request, response) {
+  response.redirect(301, "/laps/account/check-your-details")
+});
+
+// Check your details
+router.post("/laps/account/check-your-details",  function(request, response) {
+  response.redirect(301, "/laps/account/create-password")
+});
+
+// Create password
+router.post("/laps/account/create-password",  function(request, response) {
+  response.redirect(301, "/laps/account/responsibility")
+});
+
+// Choose who will manage [name]'s EPR account
+//
+// What is this person's area of responsibility?
+ router.post("/laps/account/responsibility", function (req, res) {
+  var chooseAnEvent = req.session.data["who_list"];
+
+// Finance role
+  if (chooseAnEvent === "finance")  {
+    res.redirect("/laps/account/role-details");
+  }
+// Waste role
+  if (chooseAnEvent === "waste") {
+    res.redirect("/laps/account/role-details");
+  }
+// Finance and waste role 
+  if (chooseAnEvent === "finance and waste") {
+    res.redirect("/laps/account/role-details");
+  }
+
+});
+
+// Add details
+router.post("/laps/account/role-details",  function(request, response) {
+  response.redirect(301, "/laps/account/add-another")
+});
+
+// Add another
+  router.post("/laps/account/add-another", function (req, res) {
+    req.session.upload_another = req.body.upload_another;
+    console.log(req.body);
+
+    if (req.session.upload_another === "Yes") {
+      res.redirect(301, "/laps/account/responsibility");
+    }
+ 
+    else {
+      res.redirect(301, "/laps/account/check-details");
+    }
+  });
+
+// Check details
+router.post("/laps/account/check-details",  function(request, response) {
+  response.redirect(301, "/laps/account/success")
+});
+
+ // Success page
+ router.post("/laps/account/success",  function(request, response) {
+  response.redirect(301, "/laps/account/account-home")
+});
+
+
+
+
+//
+//
+//
+// ADMIN 
+//
+//
+
 // Sign in
 router.post("/laps/admin/sign-in",  function(request, response) {
   response.redirect(301, "/laps/admin/account-home-blk")
@@ -19,7 +106,7 @@ router.post("/laps/admin/account-home-blk",  function(request, response) {
 });
 
 // upload
-router.post("/laps/admin/upload?anerror=false&uploaderror=0&",  function(request, response) {
+router.post("/laps/admin/upload",  function(request, response) {
 response.redirect(301, "/laps/admin/success")
 });
 
