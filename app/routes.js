2669dc7c3169befd8bd2ff21b5bed3b9402d37ae
simon w <simon.westcott@atos.net>
2024-02-07 15:04:02 +0000

    content updates

--- app/routes.js
+++ app/routes.js
@@ -8,61 +8,40 @@ const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
 
-// Signin
-router.post("/laps/account/signin",  function(request, response) {
-  response.redirect(301, "/laps/account/id-verification")
-});
+
 
 
 //
-// CEO Register for a LAPS Account
+// CEO Register for a LAPS Account // ========================================================================  //
 //
 
-
-
-// CEO // Set up
+// CEO Register // Set up
 router.post("/laps/account/set-up",  function(request, response) {
   response.redirect(301, "/laps/account/email")
 });
 
-// Email - old
-//router.post("/laps/account/email",  function(request, response) {
- // response.redirect(301, "/laps/account/check-your-details")
-//});
 
-
-
-
-// CEO // Email
+// CEO Register // Email
 router.post("/laps/account/email",  function(request, response) {
   response.redirect(301, "/laps/account/id-verification-ceo")
 });
 
-// CEO Check your email
-// link in page
+// CEO Register // Check your email
+// HTTP link on the page
 
-
-
-// CEO // Terms and conditions
+// CEO Register // Terms and conditions
 router.post("/laps/account/terms-and-conditions",  function(request, response) {
   response.redirect(301, "/laps/account/create-password")
 });
 
-// CEO // Create password
+// CEO Register // Create password
 router.post("/laps/account/create-password",  function(request, response) {
   response.redirect(301, "/laps/account/check-your-details")
 });
 
-
-// Check your details
-//router.post("/laps/account/check-your-details",  function(request, response) {
-//  response.redirect(301, "/laps/account/responsibility")
-// });
-
-
-// // Check your details
+// CEO Register // Check your details
 router.post("/laps/account/check-your-details", function (req, res) {
-  req.session.check_details = req.body.check_details;
+  req.session.check_details = req.body.check_details ;
   console.log(req.body);
 
   if (req.session.check_details === "Yes") {
@@ -73,35 +52,19 @@ router.post("/laps/account/check-your-details", function (req, res) {
     res.redirect(301, "/laps/account/incorrect-details");
   }
 });
-
-//
- // router.post("/laps/account/responsibility", function (req, res) {
- //  req.session.who_list = req.body.who_list;
-//    console.log(req.body);
-
- //  if (req.session.who_list === "cannot") {
- //    res.redirect(301, "/laps/account/success");
- // }
  
-  // else {
-  //   res.redirect(301, "/laps/account/role-details");
- //  }
- //});
-
- 
-//  Choose up to two people to manage 's EPR account
+// CEO Register // Choose 1 or 2 colleagues to manage [authority]'s pEPR payments and data
 
 router.post("/laps/account/responsibility",  function(request, response) {
   response.redirect(301, "/laps/account/role-details")
 });
 
-// Add details 
+// CEO Register // Enter colleagues 1's details
 router.post("/laps/account/role-details",  function(request, response) {
   response.redirect(301, "/laps/account/add-another")
 });
 
-
-// Add another
+// CEO Register // Do you want to add anyone else?
 router.post("/laps/account/add-another", function (req, res) {
   req.session.upload_another = req.body.upload_another;
   console.log(req.body);
@@ -115,152 +78,129 @@ router.post("/laps/account/add-another", function (req, res) {
      }
   });
 
-  // Add details
+// CEO Register // Enter colleague 2's details
 router.post("/laps/account/role-details-2",  function(request, response) {
   response.redirect(301, "/laps/account/check-details")
 });
 
-// Check details
+// CEO Register // Check details
 router.post("/laps/account/check-details",  function(request, response) {
   response.redirect(301, "/laps/account/success")
 });
 
- // Success page
+ // CEO Register // Success page
  router.post("/laps/account/success",  function(request, response) {
   response.redirect(301, "/laps/account/account-home-ceo")
 });
 
+//
+// <<<< END >>>> // CEO register // ======================================================================== //
+//
 
 
 
 //
-///
-///
-////
+// CEO registered with LAPs // ======================================================================== //                            
+//
 
-
-// CEO // Terms and conditions
-router.post("/laps/account/terms-and-conditions-admin",  function(request, response) {
-  response.redirect(301, "/laps/account/create-password-admin")
+// CEO registered // Set up -- new to sign in
+router.post("/laps/account/set-up-acc-ceo",  function(request, response) {
+  response.redirect(301, "/laps/account/signin")
+});
+   
+// CEO registered // Signin
+router.post("/laps/account/signin",  function(request, response) {
+  response.redirect(301, "/laps/account/id-verification")
 });
 
+// CEO registered // Check your email
+// HTTP link on the page
 
-
-// ADMIN // Create password
-router.post("/laps/account/create-password-admin",  function(request, response) {
-  response.redirect(301, "/laps/account/success-admin")
-});
-
-
-// NEW PAGE //
-router.post("/laps/account/permissions",  function(request, response) {
-  response.redirect(301, "/laps/account/role-details")
-});
-
-
-
-
-
-
-//
-//
-// 2nd person addeed
-//
-//
-
-// Choose who will manage [name]'s EPR account
-//
-// What is this person's area of responsibility?
-//router.post("/laps/account/add-another", function (req, res) {
-  //var chooseAnEvent = req.session.data["who_list_2"];
-
-// Finance role
-  //if (chooseAnEvent === "adding and amending bank details")  {
-   // res.redirect("/laps/account/role-details-2");
-  //}
-// Waste role
- // if (chooseAnEvent === "managing waste data") {
-  //  res.redirect("/laps/account/role-details-2");
- // }
-// Finance and waste role 
- // if (chooseAnEvent === "finance and waste data") {
- //   res.redirect("/laps/account/role-details-2");
- // }
-// Cant do this right now
- // if (chooseAnEvent === "no") {
- //   res.redirect("/laps/account/check-details");
- // }
-
-//});
-
-
-
-
-// Set up
-router.post("/laps/account/set-up-acc",  function(request, response) {
-  response.redirect(301, "/laps/account/account-home-ceo")
-});
-
-// Set up -- new to sign in
+// CEO registered // Set up -- new to sign in
 router.post("/laps/account/set-up-acc-ceo",  function(request, response) {
   response.redirect(301, "/laps/account/signin")
 });
 
-
-
-// CEO change user details 
+// CEO registered // CEO change user details 
 router.post("/laps/account/role-details-change",  function(request, response) {
   response.redirect(301, "/laps/account/manage-account-ceo")
 });
 
-// CEO Remove user
+// CEO registered // CEO Remove user
 router.post("/laps/account/remove",  function(request, response) {
   response.redirect(301, "/laps/account/manage-account-ceo-a")
 });
 
-// CEO add user
+// CEO registered // CEO add user
 router.post("/laps/account/manage-account-ceo-a",  function(request, response) {
   response.redirect(301, "/laps/account/responsibility-2-ceo")
 });
 
-// CEO add user responsibility
+// CEO registered // CEO add user responsibility
 router.post("/laps/account/responsibility-2-ceo",  function(request, response) {
   response.redirect(301, "/laps/account/manage-account-ceo")
 });
 
-// CEO add user details
+// CEO registered // CEO add user details
 router.post("/laps/account/role-details-2-ceo",  function(request, response) {
   response.redirect(301, "/laps/account/manage-account-ceo-a")
 });
 
+//
+// <<<< END >>>> // CEO registered // ======================================================================== //
+//
+
 
 
 
+
 //
-//
-//
-// ADMIN 
-//
+// Invited user // ======================================================================== //   
 //
 
-// Sign in
+// Invited user // Check your email
+// HTTP link on the page
+
+// Invited user // Terms and conditions
+router.post("/laps/account/terms-and-conditions-admin",  function(request, response) {
+  response.redirect(301, "/laps/account/create-password-admin")
+});
+
+// Invited user // Create a password
+router.post("/laps/account/create-password-admin",  function(request, response) {
+  response.redirect(301, "/laps/account/success-admin")
+});
+
+// Invited user  // Success
+// HTTP link on the page
+
+//
+// <<<< END >>>> // Invited user // ======================================================================== //
+//
+
+
+
+
+//
+// DEFRA ACCOUNT SUPPORT ADMIN PORTAL // ======================================================================== //
+//
+
+// DEFRA ACCOUNT SUPPORT ADMIN PORTAL // Sign in
 router.post("/laps/admin/sign-in",  function(request, response) {
   response.redirect(301, "/laps/admin/account-home-blk")
 });
 
-// account home blank
+// DEFRA ACCOUNT SUPPORT ADMIN PORTAL // account home blank
 router.post("/laps/admin/account-home-blk",  function(request, response) {
   response.redirect(301, "/laps/admin/upload")
 });
 
-// upload
+// DEFRA ACCOUNT SUPPORT ADMIN PORTAL // upload
 router.post("/laps/admin/upload",  function(request, response) {
 response.redirect(301, "/laps/admin/checked")
 });
 
-
-
-// Add another
+// DEFRA ACCOUNT SUPPORT ADMIN PORTAL // Add another
 router.post("/laps/admin/checked", function (req, res) {
   req.session.upload_file = req.body.upload_file;
   console.log(req.body);
@@ -274,7 +214,7 @@ router.post("/laps/admin/checked", function (req, res) {
   }
 });
 
-// Remove
+// DEFRA ACCOUNT SUPPORT ADMIN PORTAL // Remove
 router.post("/laps/admin/remove-barnsley", function (req, res) {
   req.session.remove_ceo = req.body.remove_ceo;
   console.log(req.body);
@@ -288,19 +228,30 @@ router.post("/laps/admin/remove-barnsley", function (req, res) {
   }
 });
 
-
-// Pause - Do you want to ...
+// DEFRA ACCOUNT SUPPORT ADMIN PORTAL // Pause - Do you want to ...
 router.post("/laps/admin/checked",  function(request, response) {
   response.redirect(301, "/laps/admin/success")
   });
   
 
-// change name
+// DEFRA ACCOUNT SUPPORT ADMIN PORTAL /// change name
 router.post("/laps/admin/barnsley-cd",  function(request, response) {
   response.redirect(301, "/laps/admin/barnsley-bcs")
 });
 
-// change name
+// DEFRA ACCOUNT SUPPORT ADMIN PORTAL // change name
 router.post("/laps/admin/barnsley-bcs",  function(request, response) {
   response.redirect(301, "/laps/admin/barnsley-bcs")
 });
+
+//
+// <<<< END >>>> // DEFRA ACCOUNT SUPPORT ADMIN PORTAL // ======================================================================== //
+//
+
+
+// NEW PAGE // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< TO BE DELETED
+router.post("/laps/account/permissions",  function(request, response) {
+  response.redirect(301, "/laps/account/role-details")
+});
+
+
